625e8150548f4949caec72cba48f1549225b8cd8
M-Naga-Shankar <logger3845@gmail.com>
2025-07-19 18:07:21 +0530

    init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
     <div className="px-8 flex items-center justify-center text-white bg-[url(/src/assets/background.jpg)] bg-no-repeat bg-cover w-full h-screen">
       {!showChat ? (
         <div className="w-fit flex flex-col justify-center items-center text-center space-y-2 bg-white/10 backdrop-blur-sm rounded-xl py-8 px-4">
-          <h1 className="text-3xl font-bold">Welcome to SocketGram</h1>
+          <h1 className="text-3xl font-bold">Welcome to Codtech-Chat</h1>
           <input
             type="text"
             placeholder="Your nickname"
